28f563eb6ef40cac657abe8f43f4b285042e608b
muthu selvam <43769553+muthu9594@users.noreply.github.com>
2023-04-14 12:13:17 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+
+
+
+
+const Connection = async(username,password) =>{
+    const URL = `mongodb://${username}:${password}@ac-uvujmqt-shard-00-00.xku1dmm.mongodb.net:27017,ac-uvujmqt-shard-00-01.xku1dmm.mongodb.net:27017,ac-uvujmqt-shard-00-02.xku1dmm.mongodb.net:27017/?ssl=true&replicaSet=atlas-ao5xjy-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    try{
+       await mongoose.connect(URL, {useNewUrlParser: true});
+       console.log("Database connected successfully");
+        }catch(error){
+                console.log("Error while connecting to database",error);
+
+
+    }
+
+
+}
+
+
+export default Connection;
